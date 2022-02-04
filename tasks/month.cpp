e4f5fb89a493e09e13ae6c26bbacc5fc632e76e1
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-02-04 07:47:54 +0000

    D

--- tasks/month.cpp
+++ tasks/month.cpp
@@ -38,56 +38,29 @@ int main()
 
   if (leap(year))
 {
-  if (month == 1)
+  if ((month % 2 != 0) && (month < 8)))
     cout << "31 Days";
+  else if ((month % 2 == 0) && (month < 8) && month != 2)
+    cout << "30 Days";
   else if (month == 2)
     cout << "29 Days";
-  else if (month == 3)
-    cout << "31 Days";
-  else if (month == 4)
+  else if (((month % 2 != 0) && (month > 8)))
     cout << "30 Days";
-  else if (month == 5)
+  else if ((month % 2 == 0) && (month > 8))
     cout << "31 Days";
-  else if (month == 6)
-    cout << "30 Days";
-  else if (month == 7)
-    cout << "31 Days";
-  else if (month == 8)
-    cout << "31 Days";
-  else if (month == 9)
-    cout << "30 Days";
-  else if (month == 10)
-    cout << "31 Days";
-  else if (month == 11)
-    cout << "30 Days";
-  else if (month == 12)
-    cout << "31 Days";
+  
 }
   else
   {
-  if (month == 1)
+    if ((month % 2 != 0) && (month < 8))
     cout << "31 Days";
+  else if ((month % 2 == 0) && (month < 8) && month != 2)
+    cout << "30 Days";
   else if (month == 2)
     cout << "28 Days";
-  else if (month == 3)
-    cout << "31 Days";
-  else if (month == 4)
+  else if (((month % 2 != 0) && (month > 8)))
     cout << "30 Days";
-  else if (month == 5)
-    cout << "31 Days";
-  else if (month == 6)
-    cout << "30 Days";
-  else if (month == 7)
-    cout << "31 Days";
-  else if (month == 8)
-    cout << "31 Days";
-  else if (month == 9)
-    cout << "30 Days";
-  else if (month == 10)
-    cout << "31 Days";
-  else if (month == 11)
-    cout << "30 Days";
-  else if (month == 12)
+  else if ((month % 2 == 0) && (month > 8))
     cout << "31 Days";
   }
 
